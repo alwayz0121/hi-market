4bfeab4b48ab0c343c9161ebdf2e04d7e24525fd
alwayz0121 <alwayz0121@gmail.com>
2025-03-15 22:24:24 +0900

    Design: login page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
-
+import { Inter } from "next/font/google";
 import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,8 +15,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="ko">
+      <body
+        className={`${inter.className} bg-gray-900 text-white max-w-sm mx-auto`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,29 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>init</div>;
+  return (
+    <div className="flex flex-col items-center justify-between min-h-screen p-6">
+      <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
+        <span className="text-9xl">👸🏻</span>
+        <h1 className="text-4xl">Princess Diary</h1>
+        <h2 className="text-2xl">전 세계 공주들을 응원해요</h2>
+      </div>
+
+      {/* 로그인 */}
+      <div className="flex flex-col items-center gap-3 w-full">
+        <Link
+          href={"/create-account"}
+          className="w-full text-white bg-yellow-500 text-lg font-medium py-2.5 rounded-md text-center hover:bg-pink-400 transition-colors"
+        >
+          시작하기
+        </Link>
+        <div className="flex gap-2">
+          <span>이미 계정이 있나요?</span>
+          <Link href={"/login"} className="hover:underline">
+            로그인
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
